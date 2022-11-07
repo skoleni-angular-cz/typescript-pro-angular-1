01a6d1f209917414fe38c26bc7c67139848218b4
skoleni-angular-cz <114166795+skoleni-angular-cz@users.noreply.github.com>
2022-11-07 21:06:44 +0100

    3 - Implemented command line user interface

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+import { mainMenu } from './screens';
 import { TaskStorage } from './task-storage';
 
 async function main() {
-  console.log('Hello TypeScript!');
-
-  // todo write it
+  try {
+    const storage = new TaskStorage();
+    await mainMenu(storage);
+  } catch (ex) {
+    if (ex instanceof Error) {
+      console.log(ex.message);
+    }
+  }
 }
 
 main();
